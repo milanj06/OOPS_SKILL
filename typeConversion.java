268f1368e9fc89dcad184b09e171774625cba78f
milanj06 <2420030789@klh.edu.in>
2025-07-23 11:35:31 +0530

    Add files via upload

---
+++ typeConversion.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class typeConversion {
+public static void main(String[] args) {
+	int n=100;
+	
+	double d =n;
+	System.out.println("Integer value: " + n);
+	System.out.println("Converted to double: " + d);
+}
+}
